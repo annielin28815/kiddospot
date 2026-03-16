21fb69f8e12ea2c25569d5e1ee535d71a39e040f
Annie <annielin28815@gmail.com>
2026-03-17 00:25:36 +0800

    chore:modify folder structure and intergration google auth api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import LoginButton from "../components/LoginButton"
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold">
         KiddoSpot
       </h1>
+
+      <LoginButton />
     </main>
   )
 }
